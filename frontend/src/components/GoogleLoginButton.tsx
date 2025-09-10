44a7cb8e938274e3c94d26cf95eeeae30cfedfd0
frog42o <jsndong03@gmail.com>
2025-09-09 22:52:14 -0400

    proj setup

---
+++ frontend/src/components/GoogleLoginButton.tsx
@@ -0,0 +1,15 @@
+import 'react'
+
+
+function GoogleLoginButton(){
+    //java script logic
+
+
+    //display view/ html css here 
+    return (<>
+        <p>google butotn here</p>
+    </>);
+}
+
+
+export default GoogleLoginButton;
