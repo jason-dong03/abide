72cd90f80764541fee25bd465e7248f3b6006ab0
Jason Dong <jsndong03@gmail.com>
2025-09-15 00:09:31 -0400

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import GoogleLoginButton from './components/GoogleLoginButton'
 
+import './App.css'
+import LandingPage from './components/LandingPage'
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <GoogleLoginButton/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        this is eyuel's version of the project abide
-      </p>
+      <LandingPage/>
     </>
   )
 }
---
+++ frontend/src/components/LandingPage.tsx
@@ -0,0 +1,18 @@
+
+
+function LandingPage(){
+
+    return(
+    <div className="container outline">
+        <div className="row">
+             <h1>abide</h1>
+            <div className="col">
+                <button className="btn btn-primary">hi</button>
+                <p>hi2</p>
+            </div>
+        </div>
+    </div>
+);
+}
+
+export default LandingPage
